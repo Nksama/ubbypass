ea69bd5a1fa980f49dbcae62ed8f61f8d6dc6a23
TechiError <error@notavailable.live>
2021-09-12 16:09:25 +0530

    Useless Hehe

--- action.sh
+++
@@ -1,6 +0,0 @@
-git clone -b dev https://github.com/TechiError/ubiquitous-octo-goggles-ultroid ultroid
-cp ultroid/.env
-cd ultroid
-ls
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
-docker run --privileged --rm -i ultroid
